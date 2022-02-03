a5906a7b2c0f9ee72fecc4bbbce6a093015a8c4c
Leticia Higino <leticialima.hn@gmail.com>
2022-02-03 15:39:56 -0300

    Consulta simples com @Query

--- src/main/java/com/leticialima/workshopmongo/config/Instantiation.java
+++ src/main/java/com/leticialima/workshopmongo/config/Instantiation.java
@@ -41,7 +41,7 @@ public class Instantiation implements CommandLineRunner {
         userRepository.save(bob);
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo.Abraços", new AuthorDTO(maria));
-        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo.Abraços", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Vou viajar para São Paulo.Abraços", new AuthorDTO(maria));
 
         CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/018"), new AuthorDTO(alex));
         CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/018"), new AuthorDTO(bob));
--- src/main/java/com/leticialima/workshopmongo/resources/PostResources.java
+++ src/main/java/com/leticialima/workshopmongo/resources/PostResources.java
@@ -1,14 +1,12 @@
 package com.leticialima.workshopmongo.resources;
 
 import com.leticialima.workshopmongo.domain.Post;
+import com.leticialima.workshopmongo.resources.util.URL;
 import com.leticialima.workshopmongo.service.PostService;
 import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,12 @@ public class PostResources {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
